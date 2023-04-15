bc770f2c552d4223220c6a856abda7f5ff432846
Artem Zaitsev <tema060701@yandex.ru>
2023-04-15 17:43:25 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"/ecommerce/controllers"
+	"/ecommerce/database"
+	"/ecommerce/middleware"
+	"/ecommerce/routes"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", controllers.GetItemFromCart())
+
+	log.Fatal(router.Run(":" + port))
+
+}
